4711345d22c244f11e29afd80ae208860ff1264b
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-11-19 15:57:58 -0600

    Partial Solution with Image

--- src/frontend/src/route/WelcomePage.js
+++ src/frontend/src/route/WelcomePage.js
@@ -1,7 +1,10 @@
+import Dustoff from "../images/Dustoff_Picture.jpeg"
+
 
 const WelcomePage = () => {
     return(
-        <div>
+        <div className={"landing-page"}>
+            <img src={Dustoff} alt={"dustoff image"}/>
             <p>Welcome to 9-Line</p>
         </div>
     )
