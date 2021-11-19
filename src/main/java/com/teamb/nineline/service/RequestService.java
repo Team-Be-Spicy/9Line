0820a987a825efbd81b4613b3ff0dbd3c71c1c62
BENNETT <75508436+cbennett07@users.noreply.github.com>
2021-11-19 15:48:03 -0600

    added request by id and request all endpoints

--- src/main/java/com/teamb/nineline/service/RequestService.java
+++ src/main/java/com/teamb/nineline/service/RequestService.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class RequestService {
@@ -13,6 +15,15 @@ public class RequestService {
 
     public Request createRequest(Request body){
         return requestRepository.save(body);
+    }
+    public Iterable<Request> listRequests(){return this.requestRepository.findAll();}
+    public Request getRequest(Long id) throws Exception{
+        Optional<Request> optionalRequest = requestRepository.findById(id);
+        try{return optionalRequest.get();}
+        catch(Exception e){
+            throw new Exception(e);
+        }
+
     }
 
 
--- src/test/java/com/teamb/nineline/controller/RequestControllerTest.java
+++ src/test/java/com/teamb/nineline/controller/RequestControllerTest.java
@@ -15,7 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -60,4 +64,80 @@ class RequestControllerTest {
 
         assertEquals(requestRepository.count(), 1);
     }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void listRequest() throws Exception {
+        Request request1 = new Request();
+        request1.setLocation("38STM1234567890");
+        request1.setResponder(null);
+        request1.setCallSign("raptor1");
+        request1.setTotalPatient(3);
+        request1.setPrecedence("urgent");
+        request1.setEquipment("hoist");
+        request1.setAmbulatory(3);
+        request1.setLitter(0);
+        request1.setMarking("smoke");
+        request1.setSecurity("clear");
+        request1.setNational("US");
+        request1.setLine9("no nbc");
+        request1.setStatus(null);
+
+        this.requestRepository.save(request1);
+
+        Request request2 = new Request();
+        request2.setLocation("22STL2345678901");
+        request2.setResponder(null);
+        request2.setCallSign("humvee7");
+        request2.setTotalPatient(3);
+        request2.setPrecedence("urgent surgical");
+        request2.setEquipment("hoist");
+        request2.setAmbulatory(2);
+        request2.setLitter(0);
+        request2.setMarking("vs panel");
+        request2.setSecurity("clear");
+        request2.setNational("UK");
+        request2.setLine9("no nbc");
+        request2.setStatus(null);
+
+        this.requestRepository.save(request2);
+
+        MockHttpServletRequestBuilder request = get("/api/request");
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].location", is("38STM1234567890")))
+                .andExpect(jsonPath("$[1].location", is("22STL2345678901")));
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void getRequestById() throws Exception {
+        Request request1 = new Request();
+        request1.setLocation("38STM1234567890");
+        request1.setResponder(null);
+        request1.setCallSign("raptor1");
+        request1.setTotalPatient(3);
+        request1.setPrecedence("urgent");
+        request1.setEquipment("hoist");
+        request1.setAmbulatory(3);
+        request1.setLitter(0);
+        request1.setMarking("smoke");
+        request1.setSecurity("clear");
+        request1.setNational("US");
+        request1.setLine9("no nbc");
+        request1.setStatus(null);
+
+        this.requestRepository.save(request1);
+
+        MockHttpServletRequestBuilder request = get("/api/request/" + request1.getId());
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.location", is("38STM1234567890")));
+    }
+
 }
