e385e036c682c629fe65c48f56e72c323bc93b44
Elliott <william.e.hamilton125.mil@swf.army.mil>
2021-11-22 11:27:42 -0600

    completed functionality for RequestList. Still needs styling.

--- src/frontend/src/route/Responder.js
+++ src/frontend/src/route/Responder.js
@@ -1,21 +1,81 @@
 import RequestList from "../component/RequestList";
 
-const requests = [{
-    id: 1,
-    status: "Pending",
-    location: "here",
-    callSign: "B12",
-    precedence: "urgent",
-    equipment: "none",
-    security: "sdadf",
-    marking: "smoke",
-    details: "view"
-}];
+const requests = [
+    {
+        id: 1,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }, {
+        id: 2,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }, {
+        id: 3,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }, {
+        id: 4,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }, {
+        id: 5,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }, {
+        id: 6,
+        status: "Pending",
+        location: "here",
+        callSign: "B12",
+        precedence: "urgent",
+        equipment: "none",
+        security: "sdadf",
+        marking: "smoke",
+        details: "view"
+    }];
 
 const Responder = () => {
+
+    const onRowSelected = (selectedIDs) => {
+        alert(selectedIDs);
+    }
+
+    const onViewClicked = (param) => {
+        alert(param);
+    }
+
     return (
-        <div>
-            <RequestList user={"responder"} requests={requests}/>
+        <div className={"Responder"}>
+            <RequestList user={"responder"} requests={requests} onRequestSelected={onRowSelected} onViewSelected={onViewClicked}/>
         </div>
     );
 }
