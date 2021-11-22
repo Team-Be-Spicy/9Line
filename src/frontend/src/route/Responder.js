e12bc3492e03679228a38a5425e3a93e5d9f4ad6
Elliott <william.e.hamilton125.mil@swf.army.mil>
2021-11-22 09:23:35 -0600

    added x-data-grid MUI dependency for RequestList shared component and built out basic structure for request list

---
+++ src/frontend/src/route/Responder.js
@@ -0,0 +1,11 @@
+import RequestList from "../component/RequestList";
+
+const Dispatcher = () => {
+    return (
+        <div>
+            <p>Dispatcher page</p>
+        </div>
+    );
+}
+
+export default Dispatcher;
