a8fb5d5e1efa4dea0049d16a5fa1dd182a19bfc1
Stephen.Scott <sscott20@gmail.com>
2021-11-22 11:54:01 -0800

    Display list view for dispatchers

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -6,6 +6,7 @@ import Responder from "./route/Responder";
 import {Link, Route, Routes} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {testBackend} from "./service/service";
+import Dispatcher from "./route/Dispatcher";
 
 const App = () => {
     const [successMessage, setSuccessMessage] = useState('');
@@ -23,12 +24,14 @@ const App = () => {
         <Link to="/">Welcome Page</Link>
         <Link to="/requester">Requester Page</Link>
         <Link to="/responder">Responder Page</Link>
+        <Link to="/dispatcher">Dispatcher Page</Link>
         <p style={{color: 'lightgreen', fontSize: '32px'}}>{successMessage}</p>
 
         <Routes>
             <Route path="/" element={<WelcomePage />} />
             <Route path="/requester" element={<Requester />} />
             <Route path="/responder" element={<Responder />} />
+            <Route path="/dispatcher" element={<Dispatcher />} />
         </Routes>
     </div>
   );
