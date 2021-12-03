25398fd02ee9f188a55e97860b64d476f17e0416
Egor <egor.krasnonosenkikh.mil@swf.army.mil>
2021-12-03 15:14:59 -0600

    fixed tets

--- src/test/java/com/teamb/nineline/controller/RequestControllerTest.java
+++ src/test/java/com/teamb/nineline/controller/RequestControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class RequestControllerTest {
@@ -68,7 +69,7 @@ class RequestControllerTest {
         mvc.perform(request)
                 .andExpect(status().isCreated());
 
-        assertEquals(requestRepository.count(), 4);
+        assertEquals(requestRepository.count(), 1);
     }
 
     @Test
